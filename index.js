5a4af9c9b9605b1d7d61e89a77d4afee07355630
MostofaFattha <mostofacse14@gmail.com>
2023-10-15 16:10:27 +0600

    FinalCommit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+let serial=0;
 // for first card 
 document.getElementById("first-card").addEventListener('click',function(){
    
-
+    serial=serial+1;
     // get the data from HTML using ID 
     const productName=document.getElementById("first-name").innerText;
     const productPrice=document.getElementById("first-price").innerText;
@@ -14,6 +15,8 @@ document.getElementById("first-card").addEventListener('click',function(){
 });
 
 document.getElementById("second-card").addEventListener('click',function(e){
+    serial=serial+1;
+
     // console.log(e.target.parentNode.parentNode.children[0].innerText);
     const pName=e.target.parentNode.parentNode.children[0].innerText;
     const pPrice=e.target.parentNode.parentNode.children[2].children[0].innerText;
@@ -23,13 +26,40 @@ document.getElementById("second-card").addEventListener('click',function(e){
 
     displayData(pName,pPrice,pQuantity,sumTotal)
 })
+// for third card 
+document.getElementById("third-card").addEventListener('click',function(){
+   
+    serial=serial+1;
+    // get the data from HTML using ID 
+    const productName=document.getElementById("third-name").innerText;
+    const productPrice=document.getElementById("third-price").innerText;
+    const productQuantity=document.getElementById("third-quantity").innerText;
+    // console.log(productName,productPrice,productQuantity);
+    const priceTotal=parseInt(productPrice)-parseInt(productQuantity);
 
+    displayData(productName,productPrice,productQuantity,priceTotal);
+    
+});
+// last card 
+document.getElementById("last-card").addEventListener('click',function(){
+   
+    serial=serial+1;
+    // get the data from HTML using ID 
+    const productName=document.getElementById("last-name").innerText;
+    const productPrice=document.getElementById("last-price").value;
+    const productQuantity=document.getElementById("last-quantity").value;
+    // console.log(productName,productPrice,productQuantity);
+    const priceTotal=parseInt(productPrice)/parseInt(productQuantity);
+
+    displayData(productName,productPrice,productQuantity,priceTotal);
+    
+});
 function displayData(nameofP,priceofP,quantityofP,resultP){
    
     const container=document.getElementById('table-container');
     const tr=document.createElement('tr');
     tr.innerHTML=`
-    <td>${1}</td>
+    <td>${serial}</td>
     <td>${nameofP}</td>
     <td>${priceofP}</td>
     <td>${quantityofP}</td>
